1956e3abd4981a878aeae94036af0ffe2c6cbf80
Sean ODonnell <odonnellsean9045@gmail.com>
2019-10-27 14:20:49 -0400

    popular styled/created battle component

--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Popular from "./components/Popular.js";
+import Popular from "./components/Popular";
+import Battle from "./components/Battle";
 
 class App extends React.Component {
   render() {
     console.log(this.props);
     return (
       <div className="container">
-        <Popular />
+        <Battle />
       </div>
     );
   }
